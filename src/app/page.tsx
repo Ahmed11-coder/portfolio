49ce984c4178453f6b36634399ef9a530b79d9a2
Ahmed <aa87006722@gmail.com>
2025-07-27 16:08:43 +0300

    ( Style ) Add Grid Background and Basic Styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
-import Image from "next/image";
-
+import Header from '@/ui/Header';
 export default function Home() {
   return (
-    <div className="container"></div>
+    <>
+      <div className='w-[100dvw] h-[100dvh] absolute bg-size-[64px_64px] bg-[linear-gradient(to_right,#00000015_1px,transparent_1px),linear-gradient(to_bottom,#00000015_1px,transparent_1px)]'>
+        <div className='w-full h-full absolute bg-[radial-gradient(transparent_0%,white_75%)]'>
+        </div>
+        <div className="w-[calc(100%-5rem)] h-[calc(100%-5rem)] relative z-1 rounded-[50px] border-[3px] border-[#EDFED6] m-10 p-10">
+          <Header />
+        </div>
+      </div>
+    </>
   );
 }
