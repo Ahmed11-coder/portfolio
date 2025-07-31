f53b056acbe9c89970429d855dac60714e5bd69e
Ahmed <aa87006722@gmail.com>
2025-07-31 18:47:25 +0300

    ( Style ) Add Pointer For Setting Icon

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -12,7 +12,7 @@ export default function Header() {
     return (
         <nav className='relative'>
             <div className='flex justify-between items-center'>
-                <h1 className={`text-4xl ${marck_script.className}`}>AH</h1>
+                <h1 className={`text-4xl ${marck_script.className} cursor-pointer`}>AH</h1>
                 <Settings strokeWidth={1} size={32} className='cursor-pointer'/>
             </div>
             <Links />
