e9514b8521dc73f4615b1988fbf96773d611eec1
Ahmed <aa87006722@gmail.com>
2025-07-31 18:49:33 +0300

    ( Refact ) Background Structure To Solve Some Layers Issues

--- src/components/layouts/Background/Background.tsx
+++ src/components/layouts/Background/Background.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import { Github, Linkedin } from 'lucide-react'
+
 import Mesh1 from './components/Mesh-Gradients/Mesh1'
 import Mesh2 from './components/Mesh-Gradients/Mesh2'
 import CutstomScrollbar from './components/Scrollbar/CutstomScrollbar'
@@ -10,11 +12,17 @@ export default function Background({Content}: {Content: React.ReactNode}) {
             <div className='w-full h-full absolute bg-[radial-gradient(transparent_0%,white_55%)]'>
             </div>
             <Mesh1 />
-            <div className="w-[calc(100%-5rem)] h-[calc(100%-5rem)] relative z-1 rounded-[50px] border-[3px] border-[#EDFED6] m-10 p-10">
-                {Content}
+            <div className="w-[calc(100%-5rem)] h-[calc(100%-5rem)] relative rounded-[50px] border-[3px] border-[#EDFED6] m-10">
+                <div className='absolute top-0 left-0 w-full h-full p-10 z-2'>
+                    {Content}
+                </div>
+                <a href="mailto:ahmedhashem2158@gmail.com" title='ahmedhashem2158@gmail.com' className='absolute font-light text-black top-52 -right-21 -rotate-90 text-sm tracking-wide z-2'>ahmedhashem2158@gmail.com</a>
+                <div className="flex flex-col gap-4 absolute bottom-28 right-4 z-2">
+                    <a href="https://github.com/Ahmed11-coder" title='My GitHub'><Github size={28} strokeWidth={1} color={"#00000080"} className='cursor-pointer'/></a>
+                    <a href="https://www.linkedin.com/in/ahmed-hashem-04117223a/" title='My LinkedIn'><Linkedin size={28} strokeWidth={1} color={"#00000080"} className='cursor-pointer'/></a>
+                </div>
             </div>
             <Mesh2 />
-            <span className='absolute font-light top-60 -right-10 z-2 -rotate-90 text-sm tracking-wide'>ahmedhashem2158@gmail.com</span>
             <hr className='absolute rounded-full bottom-16 left-16 w-0.5 h-48 bg-[#EDFED6] z-2'/>
             <CutstomScrollbar />
         </div>
