5961504b8af2dbce8c57def366309857d27171f8
Ahmed <aa87006722@gmail.com>
2025-07-31 18:07:15 +0300

    ( Style ) Imporve Mesh Gradient Styles

--- src/components/layouts/Background/components/Mesh-Gradients/Mesh1.tsx
+++ src/components/layouts/Background/components/Mesh-Gradients/Mesh1.tsx
@@ -5,7 +5,7 @@ export default function Mesh() {
     return (
         <svg width="718" height="1024" viewBox="0 0 718 1024" fill="none" xmlns="http://www.w3.org/2000/svg" className="absolute top-0 right-0 h-full">
             <g filter="url(#filter0_f_106_582)">
-                <path className="animated-path" d="M128.13 -5C118.427 50.2471 111.066 176.512 159.247 239.594C219.473 318.447 325.873 465.103 591.37 492.726C856.868 520.35 749.464 932.694 591.37 938.721C433.276 944.748 258.953 992.358 225.327 1024H759V-5H128.13Z" fill="url(#paint0_radial_106_582)"/>
+                <path className="animated-path1" d="M128.13 -5C118.427 50.2471 111.066 176.512 159.247 239.594C219.473 318.447 325.873 465.103 591.37 492.726C856.868 520.35 749.464 932.694 591.37 938.721C433.276 944.748 258.953 992.358 225.327 1024H759V-5H128.13Z" fill="url(#paint0_radial_106_582)"/>
             </g>
             <defs>
                 <filter id="filter0_f_106_582" x="0.5" y="-125.5" width="879" height="1270" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
@@ -13,9 +13,9 @@ export default function Mesh() {
                     <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
                     <feGaussianBlur stdDeviation="60" result="effect1_foregroundBlur_106_582"/>
                 </filter>
-                <radialGradient id="paint0_radial_106_582" cx="0" cy="0" r="1" gradientUnits="userSpaceOnUse" className="animated-gradient" gradientTransform="translate(388 386.5) rotate(91.0878) scale(553.1 553.1)">
+                <radialGradient id="paint0_radial_106_582" cx="0" cy="0" r="1" gradientUnits="userSpaceOnUse" className="animated-gradient1" gradientTransform="translate(388 386.5) rotate(91.0878) scale(553.1 553.1)">
                     <stop className="color-stop-1" stopColor="#D3FF94"/>
-                    <stop className="color-stop-2" offset="0.286161" stopColor="#D3FF94"/>
+                    <stop className="color-stop-2" offset="0.286161" stopColor="#BBEF87"/>
                     <stop className="color-stop-3" offset="1" stopColor="#7BC262"/>
                 </radialGradient>
             </defs>
